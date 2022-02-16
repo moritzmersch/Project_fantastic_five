39e99aa71f15dfa4c694ae5f28a960970a209953
moritzmersch <merschmannmoritz0@gmail.com>
2022-02-16 17:44:44 +0100

    merged all the solar data from wandersleben into a single csv file

---
+++ Data/Solar Wandersleben/merging_data.py
@@ -0,0 +1,17 @@
+from unittest import skip
+import pandas as pd
+import glob
+import os
+
+# merging all the csv files from the solar unit in wandersleben into a single csv file
+
+path = "/Users/mersc/Desktop/Solar_Project/Project_fantastic_five/Data/Solar Wandersleben"
+
+# joining all files that start with "20"
+all_files = glob.glob(os.path.join(path, "20*.csv"))
+
+# iterate over the files, seperate where a ";" is and skip erroneous lines
+df_from_each_file = (pd.read_csv(f, header=None, engine='python', on_bad_lines=skip, sep=';') for f in all_files)
+
+df_merged   = pd.concat(df_from_each_file, ignore_index=True)
+df_merged.to_csv( "wandersleben_merged.csv")
